442e8b6fd21ddc24912416270bb1b79318e76c1b
ltin0 <leo_tino@outlook.com.br>
2022-07-08 20:03:22 -0300

    up to git

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <BannerComp />
+  <div class="main-container">
+      <h1>Monte o seu burger</h1>
+      <BurgerForm/>
+  </div>
+</template>
+
+<script>
+import BannerComp from '@/components/BannerComp.vue';
+import BurgerForm from '@/components/BurgerForm.vue';
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    BannerComp,
+    BurgerForm
+}
+}
+</script>
